7289846d6690487c7cad734fdb31b91f480aa7d2
Khaled Hosny <khaledhosny@eglug.org>
2017-03-19 02:32:49 +0200

    Fold numtypes.h into its only user

--- source/include/extras/pubtypes.h
+++ source/include/extras/pubtypes.h
@@ -19,7 +19,39 @@ This software is licensed as OpenSource, under the Apache License, Version 2.0. 
 /***********************************************************************/
 /* Numeric definitions                                                 */
 /***********************************************************************/
-#include   "numtypes.h"
+
+/***********************************************************************/
+/* The present file assumes:                                           */
+/*                                                                     */
+/*             char:   8 bits                                          */
+/*             short: 16 bits                                          */
+/*             long:  32 bits                                          */
+/*                                                                     */
+/* This assumption is currently true for Sun, PC, and Mac environments.*/
+/* When this assumption is no longer true, #ifdef the code accordingly */
+/* and change this comment.                                            */
+/***********************************************************************/
+
+typedef long                  Fixed;
+
+
+typedef long                  Int32;
+#define MAX_INT32             ((Int32)0x7FFFFFFF)
+#define MIN_INT32             ((Int32)0x80000000)
+
+typedef unsigned long         Card32;
+#define MAX_CARD32            ((Card32)0xFFFFFFFF)
+
+typedef short                 Int16;
+#define MAX_INT16             ((Int16)0x7FFF)
+#define MIN_INT16             ((Int16)0x8000)
+
+typedef unsigned short        Card16;
+#define MAX_CARD16            ((Card16)0xFFFF)
+
+typedef unsigned char         Card8;
+#define MAX_CARD8             ((Card8)0xFF)
+
 
 typedef Int16                 *PInt16;
 typedef Int32                 *PInt32;
