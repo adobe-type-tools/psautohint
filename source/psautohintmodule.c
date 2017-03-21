7d243a5dee2bf12dfb1d0e54c10f5812b16524cc
Khaled Hosny <khaledhosny@eglug.org>
2017-03-21 22:59:04 +0200

    Add a Python module
    The API is provisional, but should get us going for now.
    
    Fixes https://github.com/khaledhosny/psautohint/issues/2

---
+++ source/psautohintmodule.c
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2014 Adobe Systems Incorporated. All rights reserved.
+ * Copyright 2017 Khaled Hosny
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use these files except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#define PY_SSIZE_T_CLEAN 1
+#include <Python.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if !defined(_MSC_VER) || _MSC_VER >= 1800
+#include <stdbool.h>
+#else
+typedef unsigned char bool;
+#define true 1
+#define false 0
+#endif
+
+#include "ac_C_lib.h"
+
+static void
+reportCB(char* msg)
+{
+  fprintf(stdout, "%s\n", msg);
+}
+
+static PyObject* PsAutoHintError;
+
+static char autohint_doc[] =
+  "Autohint glyphs.\n"
+  "\n"
+  "Signature:\n"
+  "  autohint(font_info, glyphs[, verbose, no_edit, allow_hint_sub, "
+  "round, debug])\n"
+  "\n"
+  "Args:\n"
+  "  font_info: font information.\n"
+  "  glyphs: sequence of glyph data in bez format.\n"
+  "  verbose: print verbose messages.\n"
+  "  no_edit: do not edit (change) the paths when hinting.\n"
+  "  allow_hint_sub: no multiple layers of coloring.\n"
+  "  round: round coordinates.\n"
+  "  debug: print debug messages.\n"
+  "\n"
+  "Output:\n"
+  "  Sequence of autohinted glyph data in bez format.\n"
+  "\n"
+  "Raises:\n"
+  "  psautohint.error: If authinting fails.\n";
+
+static PyObject*
+autohint(PyObject* self, PyObject* args)
+{
+  int allowEdit = true, roundCoords = true, allowHintSub = true;
+  int verbose = true;
+  int debug = false;
+  PyObject* inSeq = NULL;
+  PyObject* fontObj = NULL;
+  PyObject* outSeq = NULL;
+  int bezLen = 0;
+  char* fontInfo = NULL;
+  bool error = false;
+
+  if (!PyArg_ParseTuple(args, "O!O|iiiii", &PyBytes_Type, &fontObj, &inSeq,
+                        &verbose, &allowEdit, &allowHintSub, &roundCoords,
+                        &debug))
+    return NULL;
+
+  inSeq = PySequence_Fast(inSeq, "argument must be sequence");
+  if (!inSeq)
+    return NULL;
+
+  fontInfo = PyBytes_AsString(fontObj);
+
+  AC_SetReportCB(reportCB, verbose);
+
+  bezLen = PySequence_Fast_GET_SIZE(inSeq);
+  outSeq = PyTuple_New(bezLen);
+  if (!outSeq) {
+    error = true;
+  } else {
+    int i = 0;
+    for (i = 0; i < bezLen; i++) {
+      char* bezData = NULL;
+      char* output = NULL;
+      int outputSize = 0;
+      int result;
+
+      PyObject* itemObj = PySequence_Fast_GET_ITEM(inSeq, i);
+
+      bezData = PyBytes_AsString(itemObj);
+      if (!bezData) {
+        error = true;
+        break;
+      }
+
+      outputSize = 4 * strlen(bezData);
+      output = malloc(outputSize);
+
+      result = AutoColorString(bezData, fontInfo, output, &outputSize,
+                               allowEdit, allowHintSub, roundCoords, debug);
+      if (result == AC_DestBuffOfloError) {
+        free(output);
+        output = malloc(outputSize);
+        AC_SetReportCB(reportCB, false);
+        result = AutoColorString(bezData, fontInfo, output, &outputSize,
+                                 allowEdit, allowHintSub, roundCoords, debug);
+        AC_SetReportCB(reportCB, verbose);
+      }
+
+      if (outputSize != 0 && result == AC_Success) {
+        PyObject* bezObj = PyBytes_FromString(output);
+        PyTuple_SET_ITEM(outSeq, i, bezObj);
+      }
+
+      free(output);
+      if (result != AC_Success) {
+        PyErr_SetString(PsAutoHintError, "Hinting glyph failed");
+        error = true;
+        break;
+      }
+    }
+  }
+
+  Py_XDECREF(inSeq);
+
+  if (error) {
+    Py_XDECREF(outSeq);
+    return NULL;
+  }
+
+  return outSeq;
+}
+
+/* clang-format off */
+static PyMethodDef psautohint_methods[] = {
+  { "autohint", autohint, METH_VARARGS, autohint_doc },
+  { NULL, NULL, 0, NULL }
+};
+/* clang-format on */
+
+static char psautohint_doc[] =
+  "Python wrapper for Adobe's PostScrupt autohinter.\n"
+  "\n"
+  "autohint() -- Autohint glyphs.\n";
+
+#define SETUPMODULE                                                            \
+  PyModule_AddStringConstant(m, "version", AC_getVersion());                   \
+  PsAutoHintError = PyErr_NewException("psautohint.error", NULL, NULL);        \
+  Py_INCREF(PsAutoHintError);                                                  \
+  PyModule_AddObject(m, "error", PsAutoHintError);
+
+#if PY_MAJOR_VERSION >= 3
+/* clang-format off */
+static struct PyModuleDef psautohint_module = {
+  PyModuleDef_HEAD_INIT,
+  "psautohintmodule",
+  psautohint_doc,
+  0,
+  psautohint_methods,
+  NULL,
+  NULL,
+  NULL
+};
+/* clang-format on */
+
+PyMODINIT_FUNC
+PyInit_psautohintmodule(void)
+{
+  PyObject* m;
+
+  m = PyModule_Create(&psautohint_module);
+  if (m == NULL)
+    return NULL;
+
+  SETUPMODULE
+
+  return m;
+}
+#else /* Python < 3 */
+PyMODINIT_FUNC
+initpsautohintmodule(void)
+{
+  PyObject* m;
+
+  m = Py_InitModule3("psautohintmodule", psautohint_methods, psautohint_doc);
+  if (m == NULL)
+    return;
+
+  SETUPMODULE
+
+  return;
+}
+#endif
