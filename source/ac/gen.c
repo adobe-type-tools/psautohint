375f4f12a1d104c98f99da73abda6543c7964456
Khaled Hosny <khaledhosny@eglug.org>
2017-03-23 11:35:06 +0200

    Revert "Oops"
    This reverts commit 435effbbbca10ee91d336eb8e769c44e1cbb46f3.
    
    I’m confused. The f26b4b9555e8759dd0e444ec517aa06ecbbcf7ef commit was
    the right fix, the commit message was incorrect, though. The fix was to
    decrease the precision by rounding the numbers, the use of double made
    no difference.

--- source/ac/gen.c
+++ source/ac/gen.c
@@ -1,6 +1,8 @@
 /* Copyright 2014 Adobe Systems Incorporated (http://www.adobe.com/). All Rights Reserved.
 This software is licensed as OpenSource, under the Apache License, Version 2.0. This license is available at: http://opensource.org/licenses/Apache-2.0. */
 
+#include <math.h>
+
 #include "ac.h"
 #include "bftoac.h"
 
@@ -120,7 +122,7 @@ static bool TestBend(x0,y0,x1,y1,x2,y2) Fixed x0, y0, x1, y1, x2, y2; {
 	acfixtopflt(y2-y1, &dy2);
 	dotprod = dx1*dx2 + dy1*dy2;
 	lensqprod = (dx1*dx1 + dy1*dy1) * (dx2*dx2 + dy2*dy2);
-	return (dotprod*dotprod / lensqprod) <= .5;
+	return roundf((dotprod*dotprod / lensqprod) * 1000) / 1000 <= .5f;
 }
 
 #define TestTan(d1,d2) (abs(d1) > (abs(d2)*bendTan)/1000)
