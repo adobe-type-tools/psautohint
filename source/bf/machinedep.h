688da4ff2db17a224989f5de469635bb1f4de50c
Khaled Hosny <khaledhosny@eglug.org>
2017-03-25 00:42:37 +0200

    Cleanup more unused code

--- source/bf/machinedep.h
+++ source/bf/machinedep.h
@@ -15,56 +15,13 @@ End Edit History
 #ifndef MACHINEDEP_H
 #define MACHINEDEP_H
 
-#ifdef _WIN32
-#include <dir.h>
-#include <math.h>
-#else
-#include <sys/dir.h>
-#endif
-
-
-#define fileerror(p) ferror(p)
-
-
 extern void set_errorproc( int (*)(int16_t) );
-
-/* delimits str with / or : */
-extern void get_filename(
-    char *, char *, const char *
-);
-
-extern char *CheckBFPath(
-char *
-);
-
-extern void GetInputDirName(
-    char *, char *
-);
-
-extern uint32_t ACGetFileSize(
-    char *
-);
-
-/* Checks if the character set directory exists. */
-extern bool DirExists(
-    char *, bool, bool, bool
-);
-
-extern void  RenameFile(
-    char *, char *
-);
 
 void FlushLogFiles();
 void OpenLogFiles();
 
-extern char *GetPathName (
-   char *
-);
-
-
 #if defined(_MSC_VER) && ( _MSC_VER < 1800)
 float roundf(float x);
 #endif
-
 
 #endif /*MACHINEDEP_H*/
