6813a8d8d97bd01d56be7371f79d8400430ebd15
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-06-06 13:34:01 +0100

    setup.py: import setuptools; install package and 'autohint' script, etc
    Plus:
    
    - add fonttools to install_requires (it's imported in many places)
    - add all the metadata for uploading to PyPI.
    - set version string to 1.1.0.dev0; we shall drop the '.dev0' before tagging a the stable 1.1.0 release for deployment.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import platform
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
-module1 = Extension("_psautohint",
-                    include_dirs = [
+
+module1 = Extension("psautohint._psautohint",
+                    include_dirs=[
                         "source/include",
                     ],
-                    sources = [
+                    sources=[
                         "source/_psautohint.c",
                         "source/ac/ac.c",
                         "source/ac/ac_C_lib.c",
@@ -35,8 +36,36 @@ module1 = Extension("_psautohint",
                     ]
         )
 
-setup (name = "PsAutoHint",
-       version = "1.0",
-       description = "Python wrapper for Adobe's PostScrupt autohinter",
-       ext_modules = [module1])
-
+setup(name="psautohint",
+      version="1.1.0.dev0",
+      description="Python wrapper for Adobe's PostScript autohinter",
+      url='https://github.com/khaledhosny/psautohint',
+      author='Khaled Hosny',
+      author_email='khaledhosny@eglug.org',
+      license='Apache License, Version 2.0',
+      package_dir={'': 'python'},
+      packages=['psautohint'],
+      ext_modules=[module1],
+      entry_points={
+          'console_scripts': [
+              "autohint = psautohint.autohint:main",
+          ],
+      },
+      install_requires=[
+          'fonttools>=3.1.2',
+      ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Text Processing :: Fonts',
+          'Topic :: Multimedia :: Graphics',
+          'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+      ],
+    )
