1fb2b71ec21339a3279be30b247dec7dd92ef1c3
Khaled Hosny <khaledhosny@eglug.org>
2017-06-17 02:15:24 +0200

    Assume a recent enough MSVC is used
    This stand alone executable does not need to cope with ancient MSVC
    compiler required by Python 2.x.

--- libpsautohint/autohintexe.c
+++ libpsautohint/autohintexe.c
@@ -7,20 +7,12 @@
  * This license is available at: http://opensource.org/licenses/Apache-2.0.
  */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/stat.h>
 #include <sys/types.h>
-
-#if !defined(_MSC_VER) || _MSC_VER >= 1800
-#include <stdbool.h>
-#else
-typedef unsigned char bool;
-#define true 1
-#define false 0
-#endif
 
 #include "psautohint.h"
 
