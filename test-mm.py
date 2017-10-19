65a27b480d35933f8ba10c38761fdf3aecac355e
Khaled Hosny <khaledhosny@eglug.org>
2017-10-19 14:08:37 +0200

    Rewrite this script

--- test-mm.py
+++ test-mm.py
@@ -1,41 +1,59 @@
 from psautohint import autohint
 from psautohint import psautohint
 
-baseDir = "tests/data/source-code-pro"
-masters = ("Black", "Bold", "ExtraLight", "Light", "Medium", "Regular", "Semibold")
-
-glyphList = None
-
-fonts = []
-for master in masters:
-    print("Hinting %s" % master)
-
+def getFonts(masters, baseDir):
     options = autohint.ACOptions()
     options.quiet = True
 
-    path = "%s/%s/font.ufo" % (baseDir, master)
-    font = autohint.openUFOFile(path, "font.ufo", False, options)
-    font.useProcessedLayer = False
-    names = font.getGlyphList()
-    info = font.getFontInfo(font.getPSName(), path, False, False, [], [])
-    info = info.getFontInfo()
+    fonts = []
+    infos = []
+    for master in masters:
+        path = "%s/%s/font.ufo" % (baseDir, master)
+        font = autohint.openUFOFile(path, None, False, options)
+        font.useProcessedLayer = False
+        names = font.getGlyphList()
+        _, fontDictList = font.getfdInfo(font.getPSName(), path, False, False, [], [], names)
+        info = fontDictList[0].getFontInfo()
+        fonts.append(font)
+        infos.append(info)
 
-    if glyphList is None:
-        glyphList = names
-    else:
-        assert glyphList == names
+    return fonts, infos
 
-    glyphs = []
+def getGlyphList(fonts):
+    glyphList = fonts[0].getGlyphList()
+    assert all([font.getGlyphList() == glyphList for font in fonts])
+
+    return glyphList
+
+def mmHint(masters, fonts, infos, glyphList):
+    hinted = []
     for name in glyphList:
-        glyph = font.convertToBez(name, False, True)[0]
-        if not glyph:
-            glyph = "%%%s\n" % name
-        glyphs.append(glyph)
-    fonts.append(psautohint.autohint(info, glyphs, False, False, False))
+        glyphs = []
+        print("Hinting %s" % name)
+        for i, (font, info) in enumerate(zip(fonts, infos)):
+            glyph = font.convertToBez(name, False, True)[0]
+            if not glyph:
+                glyph = "%%%s\n" % name
+            if i == 0:
+                glyph = psautohint.autohint(info, [glyph], False, False, False, False)[0]
+            glyphs.append(glyph)
 
-glyphs = []
-for i in range(len(glyphList)):
-    glyphs.append([f[i] for f in fonts])
+        try:
+            glyphs = _psautohint.autohintmm(infos[0], [glyphs], masters, True)
+        except:
+            for i, glyph in enumerate(glyphs):
+                print(masters[i])
+                print(glyph)
+            raise
+        hinted.append(glyphs)
 
-print("MM Hinting")
-glyphs = psautohint.autohintmm(info, glyphs, masters, True)
+    return hinted
+
+def main():
+    masters = ["Black", "ExtraLight"]
+    fonts, infos = getFonts(masters, "tests/data/source-code-pro")
+    glyphList = getGlyphList(fonts)
+    hinted = mmHint(masters, fonts, infos, glyphList)
+
+if __name__ == "__main__":
+    main()
