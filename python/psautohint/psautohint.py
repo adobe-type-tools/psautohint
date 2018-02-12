eb6a49f1fccd082404828c9e18ccab0249a99e54
Miguel Sousa <msousa@adobe.com>
2018-02-12 02:04:34 -0800

    [lint] psautohint.py

--- python/psautohint/psautohint.py
+++ python/psautohint/psautohint.py
@@ -1,11 +1,13 @@
 from . import _psautohint
 
+
 def toBytes(o):
     if hasattr(o, "encode"):
         return o.encode("utf-8")
     if isinstance(o, (list, tuple)):
         return [toBytes(i) for i in o]
     return o
+
 
 def toStr(o):
     if hasattr(o, "decode"):
@@ -14,10 +16,15 @@ def toStr(o):
         return [toStr(i) for i in o]
     return o
 
+
 def autohint(info, glyphs, verbose=True, allow_edit=True, allow_hint_sub=True,
              round_ccordinates=True, debug=False):
-    hinted = _psautohint.autohint(toBytes(info), toBytes(glyphs), verbose,
-                                  allow_edit, allow_hint_sub, round_ccordinates,
+    hinted = _psautohint.autohint(toBytes(info),
+                                  toBytes(glyphs),
+                                  verbose,
+                                  allow_edit,
+                                  allow_hint_sub,
+                                  round_ccordinates,
                                   debug)
 
     return toStr(hinted)
