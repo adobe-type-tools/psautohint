93bf66bf314021258f54b8de9a33c53fd3634e11
Cosimo Lupo <cosimo@anthrotype.com>
2018-07-05 20:07:40 +0100

    config.sh: apply codacy suggestions
    https://app.codacy.com/app/frankrolf/psautohint/pullRequest?prid=1866893

--- config.sh
+++ config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
@@ -11,14 +13,15 @@ function run_tests {
     # The function is called from an empty temporary directory.
     # Get absolute path to the pre-compiled wheel
     wheelhouse=$(abspath ../wheelhouse)
-    wheel=`ls ${wheelhouse}/psautohint*.whl | head -n 1`
+    wheel=$(ls ${wheelhouse}/psautohint*.whl | head -n 1)
     if [ ! -e "${wheel}" ]; then
         echo "error: can't find wheel in ${wheelhouse} folder" 1>&2
         exit 1
     fi
 
     # select tox environment based on the current python version
-    TOXENV="py$(echo ${MB_PYTHON_VERSION} | sed 's/\.//g')-cov"
+    # E.g.: '2.7' -> 'py27-cov'
+    TOXENV="py${MB_PYTHON_VERSION//\./}-cov"
 
     # Install pre-compiled wheel and run tests against it
     tox --installpkg "${wheel}" -e "${TOXENV}"
