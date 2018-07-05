af4bfcf1077f5df2947417b7fdc04346167ae67f
Cosimo Lupo <cosimo@anthrotype.com>
2018-07-05 17:58:24 +0100

    test_hintfontsmm: reference and outputs in tmpdir, use DATA_DIR
    py.path is part of pytest (tmpdir fixture itself returns a py.path.local object)

--- tests/test_hintfontsmm.py
+++ tests/test_hintfontsmm.py
@@ -1,12 +1,17 @@
+from __future__ import print_function, division, absolute_import
+
 import glob
 import pytest
+import py.path
 
 from psautohint.autohint import ACOptions, hintFiles
 
 from .differ import main as differ
+from . import DATA_DIR
 
 
 class Options(ACOptions):
+
     def __init__(self, reference, inpaths, outpaths):
         super(Options, self).__init__()
         self.inputPaths = inpaths
@@ -16,12 +21,16 @@ class Options(ACOptions):
         self.verbose = False
 
 
-@pytest.mark.parametrize("base", glob.glob("data/*/*Masters"))
-def test_mmufo(base):
+@pytest.mark.parametrize("base", glob.glob("%s/*/*Masters" % DATA_DIR))
+def test_mmufo(base, tmpdir):
     paths = sorted(glob.glob(base + "/*.ufo"))
-    reference = paths[0]
+    # the reference font is modified in-place, make a temp copy first
+    referenceSrc = py.path.local(paths[0])
+    referenceDst = tmpdir / referenceSrc.basename
+    referenceSrc.copy(referenceDst)
+    reference = str(referenceDst)
     inpaths = paths[1:]
-    outpaths = [p + ".out" for p in inpaths]
+    outpaths = [str(tmpdir / p) for p in inpaths]
 
     options = Options(reference, inpaths, outpaths)
     hintFiles(options)
