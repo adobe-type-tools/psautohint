87e1dcb2be1b2ddabefa7f8dab157477cd965675
Cosimo Lupo <cosimo@anthrotype.com>
2018-07-06 18:50:03 +0100

    tests/__init__: minor

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import print_function, absolute_import, division
 import os
 
-ROOT = os.path.abspath(
-    os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
-)
-DATA_DIR = os.path.relpath(os.path.join(ROOT, "tests", "data"), os.getcwd())
+here = os.path.abspath(os.path.realpath(os.path.dirname(__file__)))
+DATA_DIR = os.path.relpath(os.path.join(here, "data"), os.getcwd())
