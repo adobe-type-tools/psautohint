77e1a69d95a97424ccb37cad58c506b193f2e1c9
Khaled Hosny <khaledhosny@eglug.org>
2018-07-31 01:43:32 +0200

    Start adding unit tests
    Hinted and hinted fonts/glyph data are kept booth Bez and OTF formats,
    and we test the conversion to/from Bez as well as the hinting.

---
+++ tests/unittests/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import print_function, absolute_import, division
+import os
+
+here = os.path.abspath(os.path.realpath(os.path.dirname(__file__)))
+DATA_DIR = os.path.relpath(os.path.join(here, "data"), os.getcwd())
