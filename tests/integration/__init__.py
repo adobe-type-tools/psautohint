4d60e1ef06ba626d12d0ca86aee00a2f3ef35008
Khaled Hosny <khaledhosny@eglug.org>
2018-08-05 20:57:06 +0200

    Refactor this bit of code

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -1,5 +1,13 @@
 from __future__ import print_function, absolute_import, division
 import os
+import py.path
 
 here = os.path.abspath(os.path.realpath(os.path.dirname(__file__)))
 DATA_DIR = os.path.relpath(os.path.join(here, "data"), os.getcwd())
+
+
+def make_temp_copy(tmpdir, path):
+    src = py.path.local(path)
+    dst = tmpdir / src.basename
+    src.copy(dst)
+    return str(dst)
