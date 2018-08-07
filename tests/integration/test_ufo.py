3102ab760726fea8684f3cd55be83e824a053545
Khaled Hosny <khaledhosny@eglug.org>
2018-08-07 21:33:45 +0200

    Add a test for incomplete UFO public.glyphOrder

---
+++ tests/integration/test_ufo.py
@@ -0,0 +1,13 @@
+from __future__ import print_function, division, absolute_import
+
+from psautohint.ufoFont import UFOFontData
+
+from . import DATA_DIR
+
+
+def test_incomplete_glyphorder():
+    path = "%s/dummy/incomplete_glyphorder.ufo" % DATA_DIR
+    font = UFOFontData(path, False, False, True)
+
+    assert len(font.getGlyphList()) == 95
+    assert "ampersand" in font.getGlyphList()
