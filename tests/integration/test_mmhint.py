b1271aed5f6f5d465fe9d250737d5074dac9d45a
Khaled Hosny <khaledhosny@eglug.org>
2018-08-26 00:40:47 +0200

    Add test for incompatible masters

--- tests/integration/test_mmhint.py
+++ tests/integration/test_mmhint.py
@@ -3,7 +3,7 @@ from __future__ import print_function, division, absolute_import
 import glob
 import pytest
 
-from psautohint.autohint import ACOptions, hintFiles
+from psautohint.autohint import ACOptions, ACHintError, hintFiles
 
 from .differ import main as differ
 from . import make_temp_copy, DATA_DIR
@@ -33,3 +33,16 @@ def test_mmufo(base, tmpdir):
 
     for inpath, outpath in zip(inpaths, outpaths):
         assert differ([inpath, outpath])
+
+
+def test_incompatible_masters(tmpdir):
+    base = "%s/source-serif-pro/" % DATA_DIR
+    paths = [base + "Light/font.ufo", base + "Black/font.ufo"]
+    # the reference font is modified in-place, make a temp copy first
+    reference = make_temp_copy(tmpdir, paths[0])
+    inpaths = paths[1:]
+    outpaths = [str(tmpdir / p) for p in inpaths]
+
+    options = Options(reference, inpaths, outpaths)
+    with pytest.raises(ACHintError):
+        hintFiles(options)
