adobe-type-tools/psautohint

commit 1455c356b39de9a24d42484a727d3cb408bffb1a

Use strtod in more places

libpsautohint/src/fontinfo.c
@@ -48,7 +48,7 @@ GetKeyFixedValue(const ACFontInfo* fontinfo, char* keyword, bool optional,
     fontinfostr = GetFontInfo(fontinfo, keyword, optional);
 
     if ((fontinfostr != NULL) && (fontinfostr[0] != 0)) {
-        sscanf(fontinfostr, "%g", &tempValue);
+        tempValue = strtod(fontinfostr, NULL);
         *value = (Fixed)tempValue * (1 << FixShift);
         UnallocateMem(fontinfostr);
     }
@@ -319,7 +319,7 @@ ParseIntStems(const ACFontInfo* fontinfo, char* kw, bool optional,
         while (misspace(*line))
             line++; /* skip past any blanks */
 
-        if (sscanf(line, " %d", &val) < 1)
+        if (sscanf(line, " %d", &val) == EOF)
             break;
 
         if (count >= maxstems) {

libpsautohint/src/ac.c
@@ -122,7 +122,7 @@ InitData(const ACFontInfo* fontinfo, int32_t reason)
             gListClrInfo = gDebug;
             if (gScalingHints) {
                 s = GetFontInfo(fontinfo, "OrigEmSqUnits", MANDATORY);
-                sscanf(s, "%g", &origEmSquare);
+                origEmSquare = strtod(s, NULL);
                 UnallocateMem(s);
                 gBlueFuzz = (Fixed)(origEmSquare / 2000.0); /* .5 pixel */
             } else {

libpsautohint/src/read.c
@@ -704,7 +704,7 @@ ParseString(const ACFontInfo* fontinfo, const char* s)
                 val = val * 10 + (c - '0');
             else if ((c == ' ') || (c == '\t')) {
                 if (isReal) {
-                    sscanf(c0, "%f", &rval);
+                    rval = strtod(c0, NULL);
                     rval = roundf(rval * 100) / 100; // Autohint can only
                                                      // support 2 digits of
                                                      // decimal precision.

commit 41a4785b3c826a5ecdf78c12dd9b6a98a75d85cf

Fix reading comma separated fontinfo arrays
A regression from 1455c356b39de9a24d42484a727d3cb408bffb1a.

libpsautohint/src/fontinfo.c
@@ -252,7 +252,7 @@ ParseIntStems(const ACFontInfo* fontinfo, char* kw, bool optional,
         while (misspace(*line))
             line++; /* skip past any blanks */
 
-        if (sscanf(line, " %d", &val) == EOF)
+        if (sscanf(line, " %d", &val) < 1)
             break;
 
         if (count >= maxstems) {