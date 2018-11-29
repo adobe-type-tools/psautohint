562d12bc9b37a9500152924196f845c1c6e4c2a1
Khaled Hosny <khaledhosny@eglug.org>
2018-11-29 13:02:33 +0200

    Rewrite autohint.hintFiles()
    When doing compatible hinting we want all masters, including the
    reference font, to use the output of hint_compatible_bez_glyphs().
    
    Previously we were discarding the reference master hints and always
    using its initial hinting, which can lead to incompatible masters if a
    any outline modification was done during compatible hinting, as seen in
    https://github.com/adobe-type-tools/psautohint/issues/122.
    
    As a result, we now also hint all masters at once, instead of hinting
    them one by one with the reference font. Should be a bit faster and make
    sure we always get compatible hints for all masters.

--- tests/integration/test_mmhint.py
+++ tests/integration/test_mmhint.py
@@ -1,8 +1,11 @@
 from __future__ import print_function, division, absolute_import
 
 import glob
+from os.path import basename
 import pytest
 
+from fontTools.misc.xmlWriter import XMLWriter
+from fontTools.ttLib import TTFont
 from psautohint.autohint import ACOptions, ACHintError, hintFiles
 
 from .differ import main as differ
@@ -20,19 +23,43 @@ class Options(ACOptions):
         self.verbose = False
 
 
-@pytest.mark.parametrize("base", glob.glob("%s/*/*Masters" % DATA_DIR))
+@pytest.mark.parametrize("base", glob.glob("%s/*/Masters" % DATA_DIR))
 def test_mmufo(base, tmpdir):
     paths = sorted(glob.glob(base + "/*.ufo"))
     # the reference font is modified in-place, make a temp copy first
     reference = make_temp_copy(tmpdir, paths[0])
     inpaths = paths[1:]
-    outpaths = [str(tmpdir / p) for p in inpaths]
+    outpaths = [str(tmpdir / basename(p)) + ".out" for p in inpaths]
 
     options = Options(reference, inpaths, outpaths)
     hintFiles(options)
 
     for inpath, outpath in zip(inpaths, outpaths):
         assert differ([inpath, outpath])
+
+
+@pytest.mark.parametrize("base", glob.glob("%s/*/OTFMasters" % DATA_DIR))
+def test_mmotf(base, tmpdir):
+    paths = sorted(glob.glob(base + "/*.otf"))
+    # the reference font is modified in-place, make a temp copy first
+    reference = make_temp_copy(tmpdir, paths[0])
+    inpaths = paths[1:]
+    outpaths = [str(tmpdir / basename(p)) + ".out" for p in inpaths]
+
+    options = Options(reference, inpaths, outpaths)
+    hintFiles(options)
+
+    refs = [p + ".ref" for p in paths]
+    for ref, out in zip(refs, [reference] + outpaths):
+        for path in (ref, out):
+            font = TTFont(path)
+            assert "CFF " in font
+            writer = XMLWriter(str(tmpdir / basename(path)) + ".xml")
+            font["CFF "].toXML(writer, font)
+            writer.close()
+
+        assert differ([str(tmpdir / basename(ref)) + ".xml",
+                       str(tmpdir / basename(out)) + ".xml"])
 
 
 def test_incompatible_masters(tmpdir):
