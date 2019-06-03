a2805351ddcbf1868099fc5343b1bee5062cc504
Miguel Sousa <msousa@adobe.com>
2019-06-02 23:46:26 -0700

    Glyph hashes must be recalculated when --write-default-layer option is used
    otherwise the hashes of composite glyphs are outdated

--- python/psautohint/ufoFont.py
+++ python/psautohint/ufoFont.py
@@ -429,10 +429,6 @@ class UFOFontData:
 
         writer = UFOWriter(path, self._reader.formatVersion, validate=False)
 
-        if self.hashMapChanged:
-            self.writeHashMap(writer)
-        self.hashMapChanged = False
-
         layer = PROCESSED_LAYER_NAME
         if self.writeToDefaultLayer:
             layer = None
@@ -451,9 +447,16 @@ class UFOFontData:
             if not self.writeToDefaultLayer and \
                     name in self.processedLayerGlyphMap:
                 filename = self.processedLayerGlyphMap[name]
+            # Recalculate glyph hashes
+            if self.writeToDefaultLayer:
+                self.recalcHashEntry(name, glyph)
             glyphset.contents[name] = filename
             glyphset.writeGlyph(name, glyph, glyph.drawPoints)
         glyphset.writeContents()
+
+        # Write hashmap
+        if self.hashMapChanged:
+            self.writeHashMap(writer)
 
     @property
     def hashMap(self):
@@ -486,10 +489,8 @@ class UFOFontData:
         if not hashMap:
             return  # no glyphs were processed.
 
-        hasMapKeys = hashMap.keys()
-        hasMapKeys = sorted(hasMapKeys)
         data = ["{"]
-        for gName in hasMapKeys:
+        for gName in sorted(hashMap.keys()):
             data.append("'%s': %s," % (gName, hashMap[gName]))
         data.append("}")
         data.append("")
@@ -507,6 +508,17 @@ class UFOFontData:
         # If the program is not in the history list, add it.
         if AUTOHINT_NAME not in historyList:
             historyList.append(AUTOHINT_NAME)
+
+    def recalcHashEntry(self, glyphName, glyph):
+        hashBefore, historyList = self.hashMap[glyphName]
+
+        hash_pen = HashPointPen(glyph)
+        glyph.drawPoints(hash_pen)
+        hashAfter = hash_pen.getHash()
+
+        if hashAfter != hashBefore:
+            self.hashMap[glyphName] = [tostr(hashAfter), historyList]
+            self.hashMapChanged = True
 
     def checkSkipGlyph(self, glyphName, newSrcHash, doAll):
         skip = False
